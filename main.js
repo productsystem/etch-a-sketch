faf8f36e69fc998269569dde2118de9a4e724585
productsystem <prajitpradeep@gmail.com>
2024-07-11 18:39:22 +0530

    basically everything done

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+let color = "black";
+
+
+
+
 document.addEventListener("DOMContentLoaded", function()
 {
-    createEtch(8);
+    createEtch(16);
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function()
+    {
+        let size = getSize();
+        createEtch(size);
+    })
+
 })
 
 function createEtch(size)
@@ -14,6 +26,47 @@ function createEtch(size)
     for(i = 0; i< divNum; i++)
     {
         let div = document.createElement("div");
-        
+        etch.insertAdjacentElement("beforeend", div);
+        div.addEventListener("mouseover",colorIn);
     }
+}
+
+function getSize()
+{
+    let input = prompt("Provide size for the board:");
+    let message = document.querySelector("#message");
+    if(input < 0 && input > 100)
+    {
+        message.innerHTML = "Provide number between 1 and 100";
+    }
+    else if(input > 0 && input < 100)
+    {
+        message.innerHTML = "Correct";
+        return input;
+    }
+    else
+    {
+        message.innerHTML = "Incorrect input provided";
+    }
+
+}
+
+function colorIn(){
+    if(color == "random")
+    {
+        this.style.backgroundColor = 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+    }
+    else{
+        this.style.backgroundColor = "black";
+    }
+}
+
+function setColor(choice){
+    color = choice;
+
+}
+
+function resetEtch(){
+    let divArray = document.querySelectorAll("div");
+    divArray.forEach((div) => div.style.backgroundColor = "white");
 }
